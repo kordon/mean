f6ab9969c0644150a5c2113c6f9d7cf62f3fdced
Sérgio Ramos <mail@sergioramos.me>
2013-07-08 19:31:04 +0100

    update dependencies

--- test/index.js
+++ test/index.js
@@ -1,8 +1,8 @@
 var mean = process.env.MEAN_COVERAGE ? require('../lib-cov/mean.js') : require('..'),
-    assert = require('chai').assert
+    assert = require('assert')
 
 suite('Mean');
 
-test('result', function(){
-  assert.ok(mean([111, 222, 333, 444, 555]) === 333)
+test('result', function () {
+  assert.equal(mean([111, 222, 333, 444, 555]), 333)
 })
